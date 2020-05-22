fb58f15eb301fa7d1e456d6a23906d1ec6224b2a
Melanie Bott <melaniebott@Melanies-iMac.hsd1.ut.comcast.net>
2020-05-21 22:28:17 -0600

    psuedo code each file. Tested server.js and it functioning

---
+++ server.js
@@ -0,0 +1,23 @@
+/* 
+1. Your survey should have 10 questions of your choosing. Each answer should be on a scale of 1 to 5 based on how much the user agrees or disagrees with a question.
+2. Your server.js file should require the basic npm packages we've used in class: express and path. */
+
+// Basic npm packages
+var express = require("express");
+var path = require("path");
+console.log("Did it work? Test")
+
+/* 7. Once you've found the current user's most compatible friend, display the result as a modal pop-up.
+
+The modal should display both the name and picture of the closest match.
+
+//appends an "active" class to .popup and .popup-content when the "Open" button is clicked
+$(".open").on("click", function() {
+  $(".popup-overlay, .popup-content").addClass("active");
+});
+
+//removes the "active" class to .popup and .popup-content when the "Close" button is clicked 
+$(".close, .popup-overlay").on("click", function() {
+  $(".popup-overlay, .popup-content").removeClass("active");
+}); */
+
