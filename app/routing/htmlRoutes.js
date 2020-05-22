2f770ba9639c9629dd968d662b1ae9b4932c1579
Melanie Bott <melaniebott@Melanies-iMac.hsd1.ut.comcast.net>
2020-05-21 22:56:13 -0600

    htmlRoutes file basic setup will update as other files are being created

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,4 +1,4 @@
-/* 3. Your htmlRoutes.js file should include two routes:
+/* 3. Your htmlRoutes.js file should include two routes: (delete message when done)
 
 A GET Route to /survey which should display the survey page.
 A default, catch-all route that leads to home.html which displays the home page. */
@@ -6,7 +6,9 @@ A default, catch-all route that leads to home.html which displays the home page.
 // path package for file path for html
 var path = require("path");
 
+// Routing
 modules.exports = function(app) {
+    // GET Route Requested HTML content
     app.get("/", function(req, res) {
         res.sendFile(path.join(__dirname, "../public/home.html"));
     });
